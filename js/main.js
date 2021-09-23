66f2a617cbd0ee32bda9c95369b6afc6c073554d
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-09-22 22:51:30 -0300

    Add os primeiros conteúdos.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let nome = 'Diego Santos';
+let n1 = 5;
+let n2 = 3;
+let frase = 'Japão é o melhor time do mundo';
+//alert(nome + ' tem ' + idade + ' anos.');
+//alert(idade + idade2)
+console.log(nome)
+console.log(n1 * n2)
+console.log(frase.toUpperCase()) //toLowerCase()
+console.log(frase.replace('Japão', 'Brasil'))
+//alert(frase.replace('Japão', 'Brasil'))
